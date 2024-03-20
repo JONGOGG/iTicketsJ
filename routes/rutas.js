d05a3d7fa63ac11c8ac790f34893e0b425c32565
aldayr33r <aldayrramos5@gmail.com>
2024-03-20 09:20:17 -0600

    se resolvio un problemita

--- routes/rutas.js
+++ routes/rutas.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const paginas = require('../controllers/pgController.js');
 
 router.get('/login', paginas.login)
-<<<<<<< HEAD
+
 router.get('/consultas', paginas.consultas)
-=======
+
 router.get('/registro', paginas.registro)
 router.get('/crear_ticket', paginas.crear_ticket)
 
 
->>>>>>> b396c546c9deed74668d9cb171fd1acddbc9e57b
 module.exports = router;
